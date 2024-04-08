0df6cf7924328983516c07277711a30816129a4e
Sandipan Das <sandipan.das@amd.com>
2024-04-08 22:54:40 +0530

    KVM: x86/pmu/svm: Set passthrough capability for vcpus
    Pass on the passthrough PMU setting from kvm->arch into kvm_pmu for each
    vcpu. As long as the host supports PerfMonV2, the guest PMU version does
    not matter.
    
    Signed-off-by: Sandipan Das <sandipan.das@amd.com>

--- arch/x86/kvm/svm/pmu.c
+++ arch/x86/kvm/svm/pmu.c
@@ -211,6 +211,7 @@ static void amd_pmu_refresh(struct kvm_vcpu *vcpu)
 	pmu->counter_bitmask[KVM_PMC_FIXED] = 0;
 	pmu->nr_arch_fixed_counters = 0;
 	bitmap_set(pmu->all_valid_pmc_idx, 0, pmu->nr_arch_gp_counters);
+	pmu->passthrough = vcpu->kvm->arch.enable_passthrough_pmu;
 
 	if (guest_cpuid_has(vcpu, X86_FEATURE_PERFCTR_CORE)) {
 		for (i = 0; i < pmu->nr_arch_gp_counters; i++) {
