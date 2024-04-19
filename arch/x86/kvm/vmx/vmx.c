b94ecf75d4c447d91306dd7741d1fbe850b6af4f
Mingwei Zhang <mizhang@google.com>
2024-04-19 16:41:33 +0800

    KVM: x86/pmu: Introduce enable_passthrough_pmu module parameter
    Introduce enable_passthrough_pmu as a RO KVM kernel module parameter. This
    variable is true only when the following conditions satisfies:
     - set to true when module loaded.
     - enable_pmu is true.
     - is running on Intel CPU.
     - supports PerfMon v4.
     - host PMU supports passthrough mode.
    
    The value is always read-only because passthrough PMU currently does not
    support features like LBR and PEBS, while emualted PMU does. This will end
    up with two different values for kvm_cap.supported_perf_cap, which is
    initialized at module load time. Maintaining two different perf
    capabilities will add complexity. Further, there is not enough motivation
    to support running two types of PMU implementations at the same time,
    although it is possible/feasible in reality.
    
    Finally, always propagate enable_passthrough_pmu and perf_capabilities into
    kvm->arch for each KVM instance.
    
    Signed-off-by: Mingwei Zhang <mizhang@google.com>
    Co-developed-by: Xiong Zhang <xiong.y.zhang@linux.intel.com>
    Signed-off-by: Xiong Zhang <xiong.y.zhang@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -144,6 +144,8 @@ module_param_named(preemption_timer, enable_preemption_timer, bool, S_IRUGO);
 extern bool __read_mostly allow_smaller_maxphyaddr;
 module_param(allow_smaller_maxphyaddr, bool, S_IRUGO);
 
+module_param(enable_passthrough_pmu, bool, 0444);
+
 #define KVM_VM_CR0_ALWAYS_OFF (X86_CR0_NW | X86_CR0_CD)
 #define KVM_VM_CR0_ALWAYS_ON_UNRESTRICTED_GUEST X86_CR0_NE
 #define KVM_VM_CR0_ALWAYS_ON				\
@@ -7874,13 +7876,14 @@ static u64 vmx_get_perf_capabilities(void)
 	if (boot_cpu_has(X86_FEATURE_PDCM))
 		rdmsrl(MSR_IA32_PERF_CAPABILITIES, host_perf_cap);
 
-	if (!cpu_feature_enabled(X86_FEATURE_ARCH_LBR)) {
+	if (!cpu_feature_enabled(X86_FEATURE_ARCH_LBR) &&
+	    !enable_passthrough_pmu) {
 		x86_perf_get_lbr(&lbr);
 		if (lbr.nr)
 			perf_cap |= host_perf_cap & PMU_CAP_LBR_FMT;
 	}
 
-	if (vmx_pebs_supported()) {
+	if (vmx_pebs_supported() && !enable_passthrough_pmu) {
 		perf_cap |= host_perf_cap & PERF_CAP_PEBS_MASK;
 		if ((perf_cap & PERF_CAP_PEBS_FORMAT) < 4)
 			perf_cap &= ~PERF_CAP_PEBS_BASELINE;
