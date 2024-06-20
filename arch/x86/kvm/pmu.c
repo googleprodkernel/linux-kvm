b730d43cf00c2ec657fe6ccc49cf8835fb075d94
Sandipan Das <sandipan.das@amd.com>
2024-06-20 09:09:23 +0800

    KVM: x86/pmu: Always set global enable bits in passthrough mode
    Currently, the global control bits for a vcpu are restored to the reset
    state only if the guest PMU version is less than 2. This works for
    emulated PMU as the MSRs are intercepted and backing events are created
    for and managed by the host PMU [1].
    
    If such a guest in run with passthrough PMU, the counters no longer work
    because the global enable bits are cleared. Hence, set the global enable
    bits to their reset state if passthrough PMU is used.
    
    A passthrough-capable host may not necessarily support PMU version 2 and
    it can choose to restore or save the global control state from struct
    kvm_pmu in the PMU context save and restore helpers depending on the
    availability of the global control register.
    
    [1] 7b46b733bdb4 ("KVM: x86/pmu: Set enable bits for GP counters in PERF_GLOBAL_CTRL at "RESET"");
    
    Reported-by: Mingwei Zhang <mizhang@google.com>
    Signed-off-by: Sandipan Das <sandipan.das@amd.com>
    [removed the fixes tag]

--- arch/x86/kvm/pmu.c
+++ arch/x86/kvm/pmu.c
@@ -787,7 +787,7 @@ void kvm_pmu_refresh(struct kvm_vcpu *vcpu)
 	 * in the global controls).  Emulate that behavior when refreshing the
 	 * PMU so that userspace doesn't need to manually set PERF_GLOBAL_CTRL.
 	 */
-	if (kvm_pmu_has_perf_global_ctrl(pmu) && pmu->nr_arch_gp_counters)
+	if ((pmu->passthrough || kvm_pmu_has_perf_global_ctrl(pmu)) && pmu->nr_arch_gp_counters)
 		pmu->global_ctrl = GENMASK_ULL(pmu->nr_arch_gp_counters - 1, 0);
 }
 
