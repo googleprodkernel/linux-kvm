32601fa47f3d04942509ce0d61e9c413275741da
Mingwei Zhang <mizhang@google.com>
2024-06-20 09:09:35 +0800

    KVM: x86/pmu: Introduce PMU operation prototypes for save/restore PMU context
    Plumb through kvm_pmu_ops with these two extra functions to allow pmu
    context switch.
    
    Signed-off-by: Mingwei Zhang <mizhang@google.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Tested-by: Yongwei Ma <yongwei.ma@intel.com>

--- arch/x86/include/asm/kvm-x86-pmu-ops.h
+++ arch/x86/include/asm/kvm-x86-pmu-ops.h
@@ -25,6 +25,8 @@ KVM_X86_PMU_OP_OPTIONAL(reset)
 KVM_X86_PMU_OP_OPTIONAL(deliver_pmi)
 KVM_X86_PMU_OP_OPTIONAL(cleanup)
 KVM_X86_PMU_OP_OPTIONAL(passthrough_pmu_msrs)
+KVM_X86_PMU_OP_OPTIONAL(save_pmu_context)
+KVM_X86_PMU_OP_OPTIONAL(restore_pmu_context)
 
 #undef KVM_X86_PMU_OP
 #undef KVM_X86_PMU_OP_OPTIONAL
