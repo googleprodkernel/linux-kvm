6810e3d4b67f15b86f8047fa9bd423520dd913bb
Mingwei Zhang <mizhang@google.com>
2024-11-11 08:21:10 +0000

    KVM: x86/pmu: Introduce PMU operator to check if rdpmc passthrough allowed
    Introduce a vendor specific API to check if rdpmc passthrough allowed.
    RDPMC passthrough requires guest VM have the full ownership of all
    counters. These include general purpose counters and fixed counters and
    some vendor specific MSRs such as PERF_METRICS. Since PERF_METRICS MSR is
    Intel specific, putting the check into vendor specific code.
    
    Signed-off-by: Mingwei Zhang <mizhang@google.com>
    Tested-by: Yongwei Ma <yongwei.ma@intel.com>

--- arch/x86/kvm/svm/pmu.c
+++ arch/x86/kvm/svm/pmu.c
@@ -227,6 +227,11 @@ static void amd_pmu_init(struct kvm_vcpu *vcpu)
 	}
 }
 
+static bool amd_is_rdpmc_passthru_allowed(struct kvm_vcpu *vcpu)
+{
+	return true;
+}
+
 struct kvm_pmu_ops amd_pmu_ops __initdata = {
 	.rdpmc_ecx_to_pmc = amd_rdpmc_ecx_to_pmc,
 	.msr_idx_to_pmc = amd_msr_idx_to_pmc,
@@ -236,6 +241,7 @@ struct kvm_pmu_ops amd_pmu_ops __initdata = {
 	.set_msr = amd_pmu_set_msr,
 	.refresh = amd_pmu_refresh,
 	.init = amd_pmu_init,
+	.is_rdpmc_passthru_allowed = amd_is_rdpmc_passthru_allowed,
 	.EVENTSEL_EVENT = AMD64_EVENTSEL_EVENT,
 	.MAX_NR_GP_COUNTERS = KVM_MAX_NR_AMD_GP_COUNTERS,
 	.MIN_NR_GP_COUNTERS = AMD64_NUM_COUNTERS,
