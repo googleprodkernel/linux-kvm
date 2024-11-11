18f2bca4ed5094871d59643eff136863f1aa057e
Xiong Zhang <xiong.y.zhang@linux.intel.com>
2024-11-11 08:21:24 +0000

    KVM: x86/pmu: Add support for PMU context switch at VM-exit/enter
    Add correct PMU context switch at VM_entry/exit boundary.
    
    Signed-off-by: Xiong Zhang <xiong.y.zhang@linux.intel.com>
    Tested-by: Yongwei Ma <yongwei.ma@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -10988,6 +10988,8 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 		set_debugreg(0, 7);
 	}
 
+	kvm_pmu_restore_pmu_context(vcpu);
+
 	guest_timing_enter_irqoff();
 
 	for (;;) {
@@ -11016,6 +11018,8 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 		/* Note, VM-Exits that go down the "slow" path are accounted below. */
 		++vcpu->stat.exits;
 	}
+
+	kvm_pmu_save_pmu_context(vcpu);
 
 	/*
 	 * Do this here before restoring debug registers on the host.  And
