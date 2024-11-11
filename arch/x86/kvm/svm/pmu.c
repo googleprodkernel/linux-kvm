f9c1d3f9955cf919921595e567326168cd88cf0b
Sandipan Das <sandipan.das@amd.com>
2024-11-11 08:21:31 +0000

    KVM: x86/pmu/svm: Set passthrough capability for vcpus
    Pass on the passthrough PMU setting from kvm->arch into kvm_pmu for each
    vcpu. As long as the host supports PerfMonV2, the guest PMU version does
    not matter.
    
    Note that guest vCPUs without a local APIC do not allocate an instance of
    struct kvm_lapic because of which reading the guest LVTPC before switching
    over to the PMI vector results in a NULL pointer dereference. Such vCPUs
    also cannot receive PMIs. Hence, disable passthrough mode in such cases.
    
    Signed-off-by: Sandipan Das <sandipan.das@amd.com>

--- arch/x86/kvm/svm/pmu.c
+++ arch/x86/kvm/svm/pmu.c
@@ -211,6 +211,8 @@ static void amd_pmu_refresh(struct kvm_vcpu *vcpu)
 	pmu->counter_bitmask[KVM_PMC_FIXED] = 0;
 	pmu->nr_arch_fixed_counters = 0;
 	bitmap_set(pmu->all_valid_pmc_idx, 0, pmu->nr_arch_gp_counters);
+	pmu->passthrough = vcpu->kvm->arch.enable_passthrough_pmu &&
+			   lapic_in_kernel(vcpu);
 
 	if (pmu->version > 1 || guest_cpuid_has(vcpu, X86_FEATURE_PERFCTR_CORE)) {
 		for (i = 0; i < pmu->nr_arch_gp_counters; i++) {
