310f09e2d31b8cb4861394371da1f2d16e3d8a3d
Kan Liang <kan.liang@linux.intel.com>
2024-12-23 07:13:29 +0000

    perf/x86/intel: Support PERF_PMU_CAP_MEDIATED_VPMU
    Apply the PERF_PMU_CAP_MEDIATED_VPMU for Intel core PMU. It only
    indicates that the perf side of core PMU is ready to support the
    passthrough vPMU.  Besides the capability, the hypervisor should still need
    to check the PMU version and other capabilities to decide whether to enable
    the mediated vPMU.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -4918,6 +4918,8 @@ static void intel_pmu_check_hybrid_pmus(struct x86_hybrid_pmu *pmu)
 	else
 		pmu->pmu.capabilities &= ~PERF_PMU_CAP_AUX_OUTPUT;
 
+	pmu->pmu.capabilities |= PERF_PMU_CAP_MEDIATED_VPMU;
+
 	intel_pmu_check_event_constraints(pmu->event_constraints,
 					  pmu->cntr_mask64,
 					  pmu->fixed_cntr_mask64,
@@ -6521,6 +6523,9 @@ __init int intel_pmu_init(void)
 		if (x86_pmu.intel_cap.anythread_deprecated)
 			pr_cont(" AnyThread deprecated, ");
 	}
+
+	/* The perf side of core PMU is ready to support the mediated vPMU. */
+	x86_get_pmu(smp_processor_id())->capabilities |= PERF_PMU_CAP_MEDIATED_VPMU;
 
 	/*
 	 * Install the hw-cache-events table:
