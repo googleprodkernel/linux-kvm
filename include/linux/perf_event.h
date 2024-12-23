bd9d057a30547975c5fd3a7effacedb72b830a58
Kan Liang <kan.liang@linux.intel.com>
2024-12-23 07:12:58 +0000

    perf: Support get/put mediated PMU interfaces
    Currently, the guest and host share the PMU resources when a guest is
    running. KVM has to create an extra virtual event to simulate the
    guest's event, which brings several issues, e.g., high overhead, not
    accuracy and etc.
    
    A new mediated PMU method is proposed to address the issue. It requires
    that the PMU resources can be fully occupied by the guest while it's
    running. Two new interfaces are implemented to fulfill the requirement.
    The hypervisor should invoke the interface while creating a guest which
    wants the mediated PMU capability.
    
    The PMU resources should only be temporarily occupied as a whole when a
    guest is running. When the guest is out, the PMU resources are still
    shared among different users.
    
    The exclude_guest event modifier is used to guarantee the exclusive
    occupation of the PMU resources. When creating a guest, the hypervisor
    should check whether there are !exclude_guest events in the system.
    If yes, the creation should fail. Because some PMU resources have been
    occupied by other users.
    If no, the PMU resources can be safely accessed by the guest directly.
    Perf guarantees that no new !exclude_guest events are created when a
    guest is running.
    
    Only the mediated PMU is affected, but not for other PMU e.g., uncore
    and SW PMU. The behavior of those PMUs are not changed. The guest
    enter/exit interfaces should only impact the supported PMUs.
    Add a new PERF_PMU_CAP_MEDIATED_VPMU flag to indicate the PMUs that
    support the feature.
    
    Add nr_include_guest_events to track the !exclude_guest events of PMU
    with PERF_PMU_CAP_MEDIATED_VPMU.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -301,6 +301,8 @@ struct perf_event_pmu_context;
 #define PERF_PMU_CAP_AUX_OUTPUT			0x0080
 #define PERF_PMU_CAP_EXTENDED_HW_TYPE		0x0100
 #define PERF_PMU_CAP_AUX_PAUSE			0x0200
+/* Support to passthrough whole PMU resoure to guest */
+#define PERF_PMU_CAP_MEDIATED_VPMU		0x0400
 
 /**
  * pmu::scope
@@ -1793,6 +1795,8 @@ extern void perf_event_task_tick(void);
 extern int perf_event_account_interrupt(struct perf_event *event);
 extern int perf_event_period(struct perf_event *event, u64 value);
 extern u64 perf_event_pause(struct perf_event *event, bool reset);
+int perf_get_mediated_pmu(void);
+void perf_put_mediated_pmu(void);
 #else /* !CONFIG_PERF_EVENTS: */
 static inline void *
 perf_aux_output_begin(struct perf_output_handle *handle,
@@ -1879,6 +1883,13 @@ static inline u64 perf_event_pause(struct perf_event *event, bool reset)
 {
 	return 0;
 }
+
+static inline int perf_get_mediated_pmu(void)
+{
+	return 0;
+}
+
+static inline void perf_put_mediated_pmu(void)			{ }
 #endif
 
 #if defined(CONFIG_PERF_EVENTS) && defined(CONFIG_CPU_SUP_INTEL)
